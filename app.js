31a04dbf17d498346dcc3ebb0b79df680b37b78e
eric-merritt <ericm2009@gmail.com>
2025-10-01 23:26:38 -0600

    (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   app.js

--- app.js
+++ app.js
@@ -12,11 +12,16 @@ app.use(express.static(path.join(__dirname,"./public")));
 
 app.use("/login", authController);
 
-app.get("/", (req,res) => {
-    res.render("index", {
-        header: "Open Accounts Dashboard",
-        user: { isLoggedIn: false }
-    });
+app.get("/", (req, res) => {
+  res.render("index", {
+    header: "Open Accounts Dashboard",
+    user: { isLoggedIn: !!req.user }
+  });
+});
+
+app.get("/profile", (req, res) => {
+  if (!req.user) return res.redirect("/");
+  res.render("profile", { user: req.user });
 });
 
 
